a5bd2a4dd1f6ed2855262aa9f5dc97664f630e51
Christian Alan Mendes <mendeschristian2000@gmail.com>
2024-09-09 17:36:09 -0300

    salvei

---
+++ atividade.js
@@ -0,0 +1,10 @@
+/* Seguindo a mesma estrutura de algoritimo de exemplo:
+Escreva um algoritimo que calcule a média aritimética do aluno 
+considerando as 3 notas do semestre */
+
+let numero1 = 10
+let numero2 = 10
+let numero3 = 10
+let soma = numero1+numero2+numero3
+let media = soma/3
+console.log = 30
